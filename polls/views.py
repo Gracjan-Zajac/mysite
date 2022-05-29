4904cc760f4e089670b76d985cc9fd1632b8584c
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-05-29 12:43:35 +0200

    Detail View tests

--- polls/views.py
+++ polls/views.py
@@ -15,12 +15,18 @@ class IndexView(generic.ListView):
         """Return the last five published questions (not including those set to be published in the future)."""
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        ).ordered_by('-pub_date')[:5]
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultView(generic.DetailView):
