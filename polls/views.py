b8efa9a1ceb0d6d74a030db6b574ff6555d50cfa
Gracjan-Zajac <gracjan.zajac@hotmail.com>
2022-05-31 17:17:48 +0200

    result view test

--- polls/views.py
+++ polls/views.py
@@ -33,6 +33,12 @@ class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
+    def get_queryset(self):
+        """
+        Excludes qny questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
